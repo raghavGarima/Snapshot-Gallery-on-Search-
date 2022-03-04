453935010224e24a3aefa9750cdd542d0b17c4aa
Garima Raghav <garima.raghav@pristyncare.com>
2022-03-04 15:59:00 +0530

    First Commit After Complete it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React ,{useState} from "react"
+import MainFile from "./components/MainFile";
 import './App.css';
-
+import { AppContext } from './context';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from 'react-router-dom';
 function App() {
+  const [ imagesOfGalleryArr, setImagesOfGalleryArr ] = useState([]);
+	const [typeOfComponent,setTypeOfComponent]=useState("default")
+	const dispatchUserEvent = (actionType, payload) => {
+    debugger
+		switch (actionType) {
+			case 'SET_ALL_IMAGES':
+				setImagesOfGalleryArr([...payload]);
+				return;	
+      case 'SET_COMPONENT':
+				setTypeOfComponent([...payload.type]);
+				return;
+
+			default:
+				return;
+		}
+	};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={{ imagesOfGalleryArr,typeOfComponent, dispatchUserEvent }}>
+    <Router>
+    <Routes>
+      
+        <Route exact path="/" element={<MainFile searchedItem="snow"/>}></Route>
+        <Route exact path="/mountain" element={<MainFile  searchedItem="mountain"/>}></Route>
+        <Route exact path="/beach" element={<MainFile searchedItem="beach"/>}></Route>
+        <Route exact path="/bird" element={<MainFile searchedItem="bird"/>}></Route>
+        <Route exact path="/food" element={<MainFile searchedItem="food" />}></Route>
+        <Route
+          exact
+          path="/search/:searchedItemPic"
+          element={<MainFile searchedItem="search"/>}
+        ></Route>
+   
+    </Routes>
+  </Router>
+  </AppContext.Provider>
+    
+  
   );
 }
 
